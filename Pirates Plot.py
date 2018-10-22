2d4e452ac6c77eae000b9afa76ff4cf89e6e2676
Fmilliner1999 <44092213+Fmilliner1999@users.noreply.github.com>
2018-10-22 20:56:23 +0100

    Add files via upload
    Basic plot - (Start narrative, end narrative, artifact description)
    this assumes we need to collect 6 artifacts around the map, each one pointing to the next one.
    after all 6, you encounter event after X number of moves, final event and end

---
+++ Pirates Plot.py
@@ -0,0 +1,101 @@
+'''
+~~~~ The Pirates of the approximate Atlantic and the curse of the ancient artifacts ~~~~
+
+You play as an escaped pirate's slave, who uncovers a letter from the legendary Pirate
+Hack Sparrow himself, detailing the existance of six ancient, invaluable artifacts
+whcih all guide one to the decades old horde of treasure belonging to Hack himself. You
+adventure out alone on yoru single-man sailing vessel to uncover these written secrets.
+
+Waking up on the shore of a lonely island, you barely retain the memories of your escape
+from the cruel pirate prison. As you look to your new surroundings, you see the
+lifeless bodies of two of your companions. Mourning their loss, you decide to give them a
+true burial. As you drag one body through the sand a bottle containing some
+battered parchment falls from his pocket. you uncork the bottle and read the letter inside.
+    _____                                                          ______
+   /     \________________________________________________________/      \
+  / \                                                                     \
+  |  |                                                                    |
+   \ /___________________________________________________________________/
+    \                                                                  |
+    |    For ye who follow,                                            |
+    |                                                                  |
+    |    I be dead now, which means i must pass my legacy to           |
+    |    another who shall retain the objects better than I.           |
+    |    Far across the crystal waves scattered are artifacts          |
+    |    of no one price. These six objects shall lead ye to         /
+    |    the final pass where the treasure I horded over decades      |
+    /    shall lay waiting for the arrival of one worthy. Go.         |
+    |    Claim what belongs to the oceans now, but be wary. This      |
+    |    is no task for a yellow bellied bilge rat.                   |
+    \                                                                 |
+     |   Farewell and good fortune,                                   /
+     |   Hack Narrow                                                 /
+    /                                                               |
+   /  ____ _____________________________________________________________                                                       
+   | /    \                                                             \
+   \ \__  |                                                              |
+    \____/______________________________________________________________/
+
+
+Artifact 1 - (an enscribed golden coin) It's an old gold coin. Very old, and solid gold...
+it's value has no number that is for sure. The enscribed picture is (enter location
+clue for next artifact)
+
+Artifact 2 - (An ancient greek pot) This pot details the story of perseus and medusa, an
+old Ancient Greek mythological story. On the bottom is a ragged engraving, much newer in
+age. (enter location of next artifact)
+
+Artifact 3 - (A theatherbound sailors Journal) This is a navigator's rutter. A logbook of
+directions, locations, maps and most imporantly, directions to specific areas of the deadly
+ocean. (contains directions to artifact 4)
+
+Artifact 4 - (A standard battered flintlock pistol) This pistol is common amongst pirates and
+lawmen alike, containing a single shot. Seeing the unique leather wrapped handle reminds you
+of the owner, a friend from a life almost forgotten living in (enter location).
+
+Artifact 5 - (A silver ring) When you travel to find the old friend whose pistol you found,
+you discover he has retired to become a simple shopkeep, who claims to know Hack Narrow,
+or at least he did... a long time ago. He gives you a silver ring for your travels. On the
+inside are inscribed co-ordinates (to the next artifact).
+
+Artifact 6 - The final artifact is held in a small museum in a quiet harbor town. You go
+inside, and instantly one modest exhibit catches your eye. It is the figurehead piece of
+the famed ship, Silver Sword. It is a solid silver model of a swordfish, with huge
+polished rubies for eyes. The item itself is immensely valuable, held behind thick glass
+and guarded by two armed men. It seems somwhat out of place. You read the plaque infront
+of the exhibit. "The silver figurehead of the famed ship belonged to wealthy Spanish
+businessman Carlos Buendia... of course before it was stolen from him by the enigmatic
+band of pirates known as the black mist, and handed as a prize to their captain and leader
+Garton 'No-Tongue' Crawford. It remained in his possesion until death until it was passed
+into the possession of his equally infamous but outcast son, Hack Narrow. It was in his
+period of possession where the ship was disassembled, the parts used to create some kind
+of storage room at a secret location, save this figurehead, donated to this museum."
+Shouting comes from outside the building, growing louder and louder until silenced with
+the crack of gunfire. The guards rush out to investigate. You remove your acquired pistol
+from your jacket pocket, and use the single shot to shatter the glass surrounding the
+figurehead. Apparently unnoticed due to the outisde disturbance, you carefully remove the
+left ruby eye of the figurehead and search for an exit through the back of the museum. As
+you walk away, you hear a group of men discussing something as they shuffle past the limp
+bodies of the guards into the museum.
+
+You decide to travel away from the port. (after a certain number of moves, an event
+triggers automatically, no matter where you are on the map.
+
+THE EVENT - You are sailing through calm seas and blue skies, the wind is present but not
+strong. It is peaceful. You decide to look at the ruby eye, and see that it catches
+the sunlight in a very unique manner. as you angle it differently, you see that it's
+when pointed towards the ocean, the light casts red onto the waves. The next second a wave
+hits and it's gone. You try find the same spot again, and find that it is a trail of red
+being lit up by the ruby's reflection. You change the sails and follow the path, the
+ruby lighting the way. The path finishes leading you into the mouth of a cave set just off
+a crag of rocks. Your boat fits inside just, and not too far in, you find a short wooden
+mooring dock. You step onto the rocky shore, following the tunnel into the cavern deeper.
+After a short walk on wet rock, the walls and ceilings of the tunnel start to widen, and
+the light seeping through small gaps in the rocks overhead reveal a small wooden door
+just ahead. On the door is a small hole. A slot... a lock. It fits the shape of the ruby
+perfectly as you push it into place and the door swings open. Revealed is a vast, high
+ceilinged wooden structure, small windows of light keeping the contents barely visible.
+What was visible, however, was a huge array of varied treasures, crowding the room entirely.
+The room was filled to the wooden bowed ceilings by the golden, silver, crystal treasured
+all piled together. A voice suddenly materialised from behind you, "So you found me, then".
+'''
