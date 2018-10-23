7d884966e80e685aa5bc3da244c30284e8495aa0
FieldingJD <FieldingJD@cardiff.ac.uk>
2018-10-23 19:03:33 +0100

    Added more legal words for combat

--- gameparser.py
+++ gameparser.py
@@ -1,6 +1,6 @@
 #gameparser.py
 import string
-legal_words = ["go", "attack", "take", "drop", "south", "north", "east", "west", "exit", "credits", "start", "check", "inventory", "gear", "weapons","health"]
+legal_words = ["go", "attack", "take", "drop", "south", "north", "east", "west", "exit", "credits", "start", "check", "inventory", "gear", "weapons","health","heal","fight"]
 
 def filter_words(words, legal_words):
     filtered_words = []
