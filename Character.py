b3dc2bd8d63ae464523ab6a1a2f8c460e01af522
MajorTomaso <majortomasso@gmail.com>
2018-10-23 14:49:34 +0100

    Added functions to Character

--- Character.py
+++ Character.py
@@ -9,11 +9,32 @@ class MainCharacter:
         self.weapons = weapons
 
     def printInventory(self):
-        print("Your inventory contains the following: ")
+        returnValue = "Your inventory contains the following: \n"
         for x in self.inventory:
-            print(x, ': ', self.inventory[x])
+            returnValue += "-%s : %s\n" % (x, self.inventory[x])
+        return returnValue
+        
 
+    def printGear(self):
+        returnValue = "You currently have equipped: \n"
+        for g in self.armor:
+            returnValue += "-%s, multiplies incoming damage by %d\n" %(g.name, g.armor)
+        return returnValue
 
+    def printWeapon(self):
+        returnValue = "You wield the following: \n"
+        for w in self.weapons:
+            returnValue += "-%s, damage multiplier = %d\n" %(w.name, w.damage)
+        return returnValue
+
+    def doDamage(self):
+        damage = self.damage
+        for w in self.weapons:
+            damage = damage * w.damage
+
+    def takeDamage(self, amount):
+        for g in self.armor:
+            amount = amount * g.armor
 
 class Weapon:
     def __init__(self, name, damage):
