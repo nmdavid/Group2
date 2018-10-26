0327dff9816ce1d52d70bd3fc871d2ea84d92ee0
MajorTomaso <majortomasso@gmail.com>
2018-10-26 12:40:05 +0100

    enemies are balanced

--- map.py
+++ map.py
@@ -9,21 +9,21 @@ from Character import *
 #Ships also have weapons as loot scaled to the difficulty of the enemies
 
 #These are the "easy" enemies and loot
-ShipOneEnemies = [SimpleEnemy("a Deadly Rabbit", 20, 5), SimpleEnemy("a Ferocious Rat", 10, 15), SimpleEnemy("a Big Seagull", 15, 10)]
+ShipOneEnemies = [SimpleEnemy("a Deadly Rabbit", 25, 10), SimpleEnemy("a Ferocious Rat", 28, 11), SimpleEnemy("a Big Seagull", 35, 15)]
 ShipOneDictionary = {
     "Enemies": ShipOneEnemies,
     "weaponloot" : Weapon("Pointy Stick", 2)
 }
 
 #These are the "medium" enemies and loot
-ShipTwoEnemies = [SimpleEnemy("a Spooky Scary Skeleton", 5, 20), SimpleEnemy("a Cool Cat", 7, 10), SimpleEnemy("an Octopus", 15, 20)]
+ShipTwoEnemies = [SimpleEnemy("a Spooky Scary Skeleton", 33, 15), SimpleEnemy("a Cool Cat", 35, 17), SimpleEnemy("an Octopus", 40, 20)]
 ShipTwoDictionary = {
     "Enemies": ShipTwoEnemies,
     "weaponloot": Weapon("Very Pointy Stick", 3)
 }
 
 #These are the "hard" enemies and loot
-ShipThreeEnemies = [SimpleEnemy("an Angry Pirate", 35, 10), SimpleEnemy("a Very Angry Pirate", 45, 12), SimpleEnemy("Captain Kirill", 55, 15)]
+ShipThreeEnemies = [SimpleEnemy("an Angry Pirate", 40, 30), SimpleEnemy("a Very Angry Pirate", 45, 35), SimpleEnemy("Captain Kirill", 55, 45)]
 ShipThreeDictionary = {
     "Enemies": ShipThreeEnemies,
     "weaponloot": Weapon("Flintlock Pistol", 5)
